d65594deb18f81333d545b7c87d0d42bab5df4d4
Ian Ang <iannangg02@gmail.com>
2024-10-21 23:33:27 -0700

    feat: update reset endpoint to reset all the new tables to beginning game state

--- src/api/admin.py
+++ src/api/admin.py
@@ -16,17 +16,13 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
+
     with db.engine.begin() as connection:
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = 100"))
-
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_green_potions = 0"))
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_green_ml = 0"))
-
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_red_potions = 0"))
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_red_ml = 0"))
-
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_blue_potions = 0"))
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_blue_ml = 0"))
+        connection.execute(sqlalchemy.text("UPDATE catalog SET quantity = 0"))
+        connection.execute(sqlalchemy.text("UPDATE liquid_inventory SET num_ml = 0"))
+        connection.execute(sqlalchemy.text("UPDATE cha_ching SET gold = 100"))
+        connection.execute(sqlalchemy.text("UPDATE storage_capacity SET potion_capacity = 1, ml_capacity = 1"))
+        connection.execute(sqlalchemy.text("TRUNCATE carts CASCADE"))
 
     return "OK"
 
