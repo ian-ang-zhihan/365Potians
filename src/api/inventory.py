125ccec2d30a16c33227fae626120d7884f64c48
Ian Ang <iannangg02@gmail.com>
2024-10-21 23:17:56 -0700

    feat: added ability to upgrade storage capacity

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -15,34 +15,15 @@ router = APIRouter(
 def get_inventory():
     """ """
 
-    """
-    SELECT SUM(quantity)
-    FROM catalog
-    JOIN???
-
-    SELECT SUM(num_ml)
-    FROM liquid_inventory
-
-    SELECT gold
-    FROM cha_ching
-    
-
-    """
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT SUM(quantity) FROM catalog")).mappings()
-        # print("SUM(quantity) result = ", result)
-        potion_inventory = result.fetchone()
+        potion_inventory = connection.execute(sqlalchemy.text("SELECT SUM(quantity) FROM catalog")).mappings().fetchone()
         # print("potion_inventory = ", potion_inventory)
         # print("potion_inventory[\"sum\"] = ", potion_inventory["sum"])
 
-        result = connection.execute(sqlalchemy.text("SELECT SUM(num_ml) FROM liquid_inventory")).mappings()
-        # print("SUM(num_ml) result = ", result)
-        liquid_inventory = result.fetchone()
+        liquid_inventory = connection.execute(sqlalchemy.text("SELECT SUM(num_ml) FROM liquid_inventory")).mappings().fetchone()
         # print("liquid_inventory = ", liquid_inventory)
 
-        result = connection.execute(sqlalchemy.text("SELECT gold FROM cha_ching")).mappings()
-        # print("gold result = ", result)
-        gold_inventory = result.fetchone()
+        gold_inventory = connection.execute(sqlalchemy.text("SELECT gold FROM cha_ching")).mappings().fetchone()
         # print("gold_inventory = ", gold_inventory)
 
     return {"number_of_potions": potion_inventory["sum"], "ml_in_barrels": liquid_inventory["sum"], "gold": gold_inventory["gold"]}
@@ -55,9 +36,23 @@ def get_capacity_plan():
     capacity unit costs 1000 gold.
     """
 
+    with db.engine.begin() as connection:
+        cur_capacity = connection.execute(sqlalchemy.text("SELECT potion_capacity, ml_capacity FROM storage_capacity")).mappings().fetchone()
+        potion_inventory = connection.execute(sqlalchemy.text("SELECT SUM(quantity) FROM catalog")).mappings().fetchone()
+        liquid_inventory = connection.execute(sqlalchemy.text("SELECT SUM(num_ml) FROM liquid_inventory")).mappings().fetchone()
+        gold_inventory = connection.execute(sqlalchemy.text("SELECT gold FROM cha_ching")).mappings().fetchone()
+
+    potion_capacity_to_add = 0
+    ml_capacity_to_add = 0
+    if potion_inventory["sum"] >= (40 * cur_capacity["potion_capacity"]) and gold_inventory["gold"] >= 1000:
+        potion_capacity_to_add += 1
+
+    if liquid_inventory["sum"] >= (9000 * cur_capacity["ml_capacity"]) and gold_inventory["gold"] >= 1000:
+        ml_capacity_to_add += 1
+
     return {
-        "potion_capacity": 0,
-        "ml_capacity": 0
+        "potion_capacity": potion_capacity_to_add,
+        "ml_capacity": ml_capacity_to_add
         }
 
 class CapacityPurchase(BaseModel):
@@ -71,5 +66,17 @@ def deliver_capacity_plan(capacity_purchase : CapacityPurchase, order_id: int):
     Start with 1 capacity for 50 potions and 1 capacity for 10000 ml of potion. Each additional 
     capacity unit costs 1000 gold.
     """
+    capacity_upgrade_parameters = [
+        {
+            "potion_capacity_upgrade" : capacity_purchase.potion_capacity,
+            "ml_capacity_upgrade" : capacity_purchase.ml_capacity
+        }
+    ]
+
+    cost = {"cost" : (capacity_purchase.potion_capacity + capacity_purchase.ml_capacity) * 1000}
+
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("UPDATE storage_capacity SET potion_capacity = potion_capacity + :potion_capacity_upgrade, ml_capacity = ml_capacity + :ml_capacity_upgrade"), capacity_upgrade_parameters)
+        connection.execute(sqlalchemy.text("UPDATE cha_ching SET gold = gold - :cost"), cost)
 
     return "OK"
