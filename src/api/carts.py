62f2beeb978ac11e15aed9501111c719e8cb8941
Ian Ang <iannangg02@gmail.com>
2024-10-25 18:38:03 -0700

    fix: neatened up code by removing a ton of redundant comments

--- src/api/carts.py
+++ src/api/carts.py
@@ -93,30 +93,16 @@ def post_visits(visit_id: int, customers: list[Customer]):
 
     return "OK"
 
-# TODO: convert this to a table in the database
-cart_id = 0
-carts = {} # {id : {item_sku : quantity}}
-
 
 @router.post("/")
 def create_cart(new_cart: Customer):
     """ """
-    # global cart_id
-    # global carts
-
-
     """
     {
   "customer_name": "Valiant Hadrian Ironhart",
   "character_class": "Paladin",
   "level": 5
     }
-
-    1. INSERT INTO carts FIRST
-    2. THEN INSERT INTO customer_purchases
-    MAYBE THIS SHOULDN'T EVEN BE IN THE CREATE CART FUNCTION
-    insert_parameters.append({"customer_name" : new_cart.customer_name})
-
     """
 
     insert_parameters = []
@@ -132,15 +118,7 @@ def create_cart(new_cart: Customer):
         print("insert_parameter = ", insert_parameters)
         connection.execute(sqlalchemy.text("INSERT INTO customer_purchases (cart_id, customer_id) SELECT :cart_id, customer_visits.id FROM customer_visits WHERE customer_visits.customer_name = :customer_name AND customer_visits.character_class = :character_class"), insert_parameters)
         
-
-    # cart_id += 1
-    # if cart_id not in carts:
-    #     carts[cart_id] = {}
-
-    # for cart in carts.items():
-    #     print("Customer = ", new_cart)
-    #     print("created")
-    #     print("Cart = ", cart)
+        # TODO: instead of just identifying by name, try identifying by name, class & level
 
     return {
         "cart_id": cart_id
@@ -154,7 +132,6 @@ class CartItem(BaseModel):
 @router.post("/{cart_id}/items/{item_sku}")
 def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     """ """
-    # global carts
 
     print("cart_item = ", item_sku, cart_item)
 
@@ -165,22 +142,8 @@ def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
         "quantity" : cart_item.quantity
     })
 
-    """
-    INSERT INTO cart_items (cart_id, catalog_id, potion_quantity)
-    SELECT :cart_id, catalog_id, :quantity
-    FROM catalog
-    WHERE potion_sku = :item_sku
-    000R000G100B000D
-    """
-
     with db.engine.begin() as connection:
         connection.execute(sqlalchemy.text("INSERT INTO cart_items (cart_id, catalog_id, potion_quantity) SELECT :cart_id, catalog_id, :quantity FROM catalog WHERE potion_sku = :item_sku"), insert_parameters)
-
-    # if cart_id in carts:
-    #     carts[cart_id][item_sku] = cart_item.quantity
-
-    # for cart in carts.items():
-    #     print(cart)
 
     return "OK"
 
@@ -192,27 +155,15 @@ class CartCheckout(BaseModel):
 def checkout(cart_id: int, cart_checkout: CartCheckout):
     """ """
     print("cart_checkout.payment = ", cart_checkout.payment)
-    
-    # global carts
-
-    """
-    SELECT potion_price, cart_items.quantity
-    FROM cart_items
-    JOIN catalog ON catalog. catalog_id = cart_items.catalog_id
-    WHERE cart_id = :cart_id
-    """
 
     with db.engine.begin() as connection:
-        # result = connection.execute(sqlalchemy.text(f"SELECT num_green_potions, num_red_potions, num_blue_potions, gold FROM global_inventory")).mappings()
         result = connection.execute(sqlalchemy.text("SELECT catalog.catalog_id, potion_price, cart_items.potion_quantity FROM cart_items JOIN catalog ON catalog.catalog_id = cart_items.catalog_id WHERE cart_id = :cart_id"), {"cart_id" : cart_id}).mappings().fetchall()
         print("result = ", result)
 
         
     total_potions_bought = 0
     revenue = 0
-    potion_types_bought = []
     catalog_update_parameters = []
-    # cha_ching_update_parameters = []
 
     for item in result:
         total_potions_bought += item["potion_quantity"]
@@ -222,62 +173,10 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
                                           "potion_quantity" : item["potion_quantity"],
                                           "cart_id" : cart_id})
         
-    # cha_ching_update_parameters.append({"revenue" : revenue})
     print("catalog_update_parameters = ", catalog_update_parameters)
-
-    """
-    UPDATE catalog
-    SET catalog.quantity = catalog.quantity - :quantity
-    FROM cart_items
-    WHERE catalog.catalog_id = cart_items.catalog_id AND cart_items.cart_id = :cart_id
-
-    UPDATE cha_ching
-    SET gold = gold + :revenue
-    """
 
     with db.engine.begin() as connection:
         connection.execute(sqlalchemy.text("UPDATE catalog SET quantity = catalog.quantity - :potion_quantity FROM cart_items WHERE catalog.catalog_id = :catalog_id AND cart_items.cart_id = :cart_id"), catalog_update_parameters)
         connection.execute(sqlalchemy.text("UPDATE cha_ching SET gold = gold + :revenue"), {"revenue" : revenue})
-        
-    """
-    inventory = result.fetchone()
-
-    cur_green_potions = inventory["num_green_potions"]
-    cur_red_potions = inventory["num_red_potions"]
-    cur_blue_potions = inventory["num_blue_potions"]
-    cur_gold = inventory["gold"]
-
-    for potion_sku, quantity in carts[cart_id].items():
-        # total_potions_bought += potion
-        if "GREEN" in potion_sku:
-            if cur_green_potions >= quantity:
-                revenue += (quantity * 50)
-                total_potions_bought += quantity
-
-                cur_green_potions -= quantity
-
-        if "RED" in potion_sku:
-            if cur_red_potions >= quantity:
-                revenue += (quantity * 50)
-                total_potions_bought += quantity
-                cur_red_potions -= quantity
-
-        if "BLUE" in potion_sku:
-            if cur_blue_potions >= quantity:
-                revenue += (quantity * 60)
-                total_potions_bought += quantity
-                cur_blue_potions -= quantity
-
-    new_gold = cur_gold + revenue
-
-    with db.engine.begin() as connection:
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_green_potions = {cur_green_potions}"))
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_red_potions = {cur_red_potions}"))
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_blue_potions = {cur_blue_potions}"))
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = {new_gold}"))
-
-    # TODO: Update your database after they checkout for minus potions & add gold
-    # TODO: you get the item sku & price from your catalog to compute the total
-    """
 
     return {"total_potions_bought": total_potions_bought, "total_gold_paid": revenue}
