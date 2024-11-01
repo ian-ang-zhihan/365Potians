dd47317601625c3a4ed33327fa42f3a7280a2c78
Ian Ang <iannangg02@gmail.com>
2024-11-01 10:59:38 -0700

    feat: updated to get top 6 potions from catalog based on v4 ledger version

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -16,8 +16,17 @@ def get_catalog():
     - check API Spec to ensure you're returning the right thing
     """
 
+    sql_to_execute = """
+                        SELECT potion_sku, potion_name, potion_price, catalog.potion_type, SUM(potion_entries.potion_change) AS potion_quantity
+                        FROM catalog
+                        JOIN potion_entries ON catalog.potion_type = potion_entries.potion_type
+                        GROUP BY potion_sku, potion_name, potion_price, catalog.potion_type
+                        HAVING SUM(potion_entries.potion_change) > 0
+                        ORDER BY SUM(potion_entries.potion_change) DESC
+                        LIMIT 6
+                    """
     with db.engine.begin() as connection:
-        catalog_inventory = connection.execute(sqlalchemy.text("SELECT potion_sku, potion_name, quantity, potion_price, potion_type FROM catalog WHERE quantity > 0")).mappings().fetchall()
+        catalog_inventory = connection.execute(sqlalchemy.text(sql_to_execute)).mappings().fetchall()
 
         print("catalog_inventory = ", catalog_inventory)
 
