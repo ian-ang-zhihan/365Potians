d3d12c73d56cb1af8c06d43df41126a48b14b60c
Ian Ang <iannangg02@gmail.com>
2024-11-02 12:50:09 -0700

    feat: update bottler.py to ledgerized design

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -37,10 +37,13 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
     potion_update_parameters = []
 
     red_used = green_used = blue_used = dark_used = 0
+
     for potion in potions_delivered:
         potion_update_parameters.append({
-            "potion_quantity" : potion.quantity,
-            "potion_type" : potion.potion_type
+            "transaction_id" : order_id,
+            "transaction_type" : "POTION_BOTTLE",
+            "potion_type" : potion.potion_type,
+            "potion_change" : potion.quantity
         })
 
         red_used += (potion.potion_type[0] * potion.quantity)
@@ -49,6 +52,7 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
         dark_used += (potion.potion_type[3] * potion.quantity)
 
     print("potion_update_parameters = ", potion_update_parameters)
+
     print("red_used = ", red_used)
     print("green_used = ", green_used)
     print("blue_used = ", blue_used)
@@ -56,27 +60,43 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
 
     liquid_update_parameters = [
         {
-            "color" : "RED",
-            "ml_used" : red_used
+            "transaction_id" : order_id,
+            "transaction_type" : "POTION_BOTTLE",
+            "potion_type" : [1, 0, 0, 0],
+            "liquid_change" : -red_used if red_used > 0 else 0
         },
         {
-            "color" : "GREEN",
-            "ml_used" : green_used
+            "transaction_id" : order_id,
+            "transaction_type" : "POTION_BOTTLE",
+            "potion_type" : [0, 1, 0, 0],
+            "liquid_change" : -green_used if green_used > 0 else 0
         },
         {
-            "color" : "BLUE",
-            "ml_used" : blue_used
+            "transaction_id" : order_id,
+            "transaction_type" : "POTION_BOTTLE",
+            "potion_type" : [0, 0, 1, 0],
+            "liquid_change" : -blue_used if blue_used > 0 else 0
         },
         {
-            "color" : "DARK",
-            "ml_used" : dark_used
+            "transaction_id" : order_id,
+            "transaction_type" : "POTION_BOTTLE",
+            "potion_type" : [0, 0, 0, 1],
+            "liquid_change" : -dark_used if dark_used > 0 else 0
         }
     ]
 
-
     with db.engine.begin() as connection:
-        connection.execute(sqlalchemy.text("UPDATE catalog SET quantity = catalog.quantity + :potion_quantity WHERE potion_type = :potion_type"), potion_update_parameters)
-        connection.execute(sqlalchemy.text("UPDATE liquid_inventory SET num_ml = num_ml - :ml_used WHERE color = :color"), liquid_update_parameters)
+        sql_to_execute = """
+                            INSERT INTO potion_entries (transaction_id, transaction_type, potion_type, potion_change) VALUES
+                            (:transaction_id, :transaction_type, :potion_type, :potion_change)
+                         """
+        connection.execute(sqlalchemy.text(sql_to_execute), potion_update_parameters)
+
+        sql_to_execute = """
+                            INSERT INTO barrel_entries (transaction_id, transaction_type, potion_type, liquid_change) VALUES 
+                            (:transaction_id, :transaction_type, :potion_type, :liquid_change)
+                         """
+        connection.execute(sqlalchemy.text(sql_to_execute), liquid_update_parameters)
 
     return "OK"
 
@@ -99,17 +119,28 @@ def get_bottle_plan():
     """
 
     with db.engine.begin() as connection:
-        potion_inventory = connection.execute(sqlalchemy.text("SELECT potion_type, quantity FROM catalog WHERE quantity < 10")).mappings().fetchall()
+        sql_to_execute = """
+                            SELECT potion_type, COALESCE(SUM(potion_entries.potion_change), 0) AS quantity
+                            FROM potion_entries
+                            GROUP BY potion_type
+                         """
+        potion_inventory = connection.execute(sqlalchemy.text(sql_to_execute)).mappings().fetchall()
         print("potion_inventory = ", potion_inventory)
 
-        liquid_inventory = connection.execute(sqlalchemy.text("SELECT potion_type, color, num_ml FROM liquid_inventory")).mappings().fetchall()
+        sql_to_execute = """
+                            SELECT barrels.potion_type, barrels.color, COALESCE(SUM(barrel_entries.liquid_change), 0) AS num_ml
+                            FROM barrel_entries
+                            LEFT JOIN barrels ON barrel_entries.potion_type = barrels.potion_type
+                            GROUP BY barrels.potion_type, barrels.color
+                         """
+        liquid_inventory = connection.execute(sqlalchemy.text(sql_to_execute)).mappings().fetchall()
         print("liquid_inventory = ", liquid_inventory)
 
-        """
-        SELECT SUM(potion_capacity) AS potion_capacity, SUM(ml_capacity) AS ml_capacity
-        FROM storage_capacity
-        """
-        cur_capacity = connection.execute(sqlalchemy.text("SELECT potion_capacity, ml_capacity FROM storage_capacity")).mappings().fetchone()
+        sql_to_execute = """
+                            SELECT SUM(potion_capacity) AS potion_capacity, SUM(ml_capacity) AS ml_capacity
+                            FROM storage_capacity
+                         """
+        cur_capacity = connection.execute(sqlalchemy.text(sql_to_execute)).mappings().fetchone()
 
     minH = []
     cur_potion_inventory = 0
@@ -121,29 +152,29 @@ def get_bottle_plan():
     heapq.heapify(minH)
     print("minH = ", minH)
 
+    red_available = green_available = blue_available = dark_available = 0
     for liquid in liquid_inventory:
         if liquid["color"] == "RED":
-            red_available = liquid["num_ml"]
+            red_available += liquid["num_ml"]
         if liquid["color"] == "GREEN":
-            green_available = liquid["num_ml"]
+            green_available += liquid["num_ml"]
         if liquid["color"] == "BLUE":
-            blue_available = liquid["num_ml"]
+            blue_available += liquid["num_ml"]
         if liquid["color"] == "DARK":
-            dark_available = liquid["num_ml"]
+            dark_available += liquid["num_ml"]
 
     bottle_plan = []
 
     max_potion_capacity = 50 * cur_capacity["potion_capacity"]
     print("max_potion_capacity = ", max_potion_capacity)
+
+    available_potion_capacity = max_potion_capacity - cur_potion_inventory
+
     while minH:
         potion_quantity, potion_type = heapq.heappop(minH)
 
+        print("available_potion_capacity = ", available_potion_capacity)
         quantity_to_bottle = 0
-
-        # red_needed = node[1][0]
-        # green_needed = node[1][1]
-        # blue_needed = node[1][2]
-        # dark_needed = node[1][3]
 
         red_needed, green_needed, blue_needed, dark_needed = potion_type
 
@@ -158,7 +189,7 @@ def get_bottle_plan():
             green_available >= green_needed and 
             blue_available >= blue_needed and 
             dark_available >= dark_needed and 
-            quantity_to_bottle + cur_potion_inventory < max_potion_capacity
+            quantity_to_bottle < available_potion_capacity
             ):
             quantity_to_bottle += 1
             red_available -= red_needed
@@ -166,19 +197,18 @@ def get_bottle_plan():
             blue_available -= blue_needed
             dark_available -= dark_needed
 
-            if quantity_to_bottle == max_potion_capacity // 2:
+            if quantity_to_bottle == available_potion_capacity // 2:
                 break
         
         print("quantity_to_bottle = ", quantity_to_bottle)
-        print("cur_potion_inventory = ", cur_potion_inventory)
 
-        if quantity_to_bottle > 0:
+        if quantity_to_bottle > 0 and quantity_to_bottle <= available_potion_capacity:
             bottle_plan.append({
                 "potion_type" : potion_type,
                 "quantity" : quantity_to_bottle
             })
 
-            cur_potion_inventory += quantity_to_bottle
+            available_potion_capacity -= quantity_to_bottle
     
     print("bottle_plan = ", bottle_plan)
 
