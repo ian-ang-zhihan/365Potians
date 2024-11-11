4a8362234cc1ac4664092ff7f8414b556a3bce2b
Ian Ang <iannangg02@gmail.com>
2024-11-11 11:25:25 -0800

    update: added initial data population for barrel_entries and potion_entries

--- src/api/admin.py
+++ src/api/admin.py
@@ -33,8 +33,33 @@ def reset():
 
         connection.execute(sqlalchemy.text("TRUNCATE carts CASCADE"))
         connection.execute(sqlalchemy.text("TRUNCATE potion_entries, barrel_entries, cha_ching_entries, capacity_entries"))
+        
         connection.execute(sqlalchemy.text("INSERT INTO cha_ching_entries (transaction_id, transaction_type, cha_change) VALUES (0, 'BASE', 100)"))
+
         connection.execute(sqlalchemy.text("INSERT INTO capacity_entries (transaction_id, transaction_type, potion_capacity, ml_capacity) VALUES (0, 'BASE', 1, 1)"))
+        
+        potion_population = """
+                                INSERT INTO potion_entries (transaction_id, transaction_type, potion_type, potion_change) VALUES
+                                (1, 'BASE', '{100, 0, 0, 0}', 0),
+                                (1, 'BASE', '{0, 100, 0, 0}', 0),
+                                (1, 'BASE', '{0, 0, 100, 0}', 0),
+                                (1, 'BASE', '{0, 0, 0, 100}', 0),
+                                (1, 'BASE', '{50, 50, 0, 0}', 0),
+                                (1, 'BASE', '{50, 0, 50, 0}', 0),
+                                (1, 'BASE', '{0, 50, 50, 0}', 0),
+                                (1, 'BASE', '{25, 25, 25, 25}', 0);
+                            """
+
+        connection.execute(sqlalchemy.text(potion_population))
+
+        barrel_population = """
+                                INSERT INTO barrel_entries (transaction_id, transaction_type, potion_type, liquid_change) VALUES
+                                (1, 'BASE', '{1, 0, 0, 0}', 0),
+                                (1, 'BASE', '{0, 1, 0, 0}', 0),
+                                (1, 'BASE', '{0, 0, 1, 0}', 0),
+                                (1, 'BASE', '{0, 0, 0, 1}', 0);
+                            """
+        connection.execute(sqlalchemy.text(barrel_population))
 
     return "OK"
 
